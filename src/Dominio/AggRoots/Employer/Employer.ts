720c2b94cde31ee90fa1b1dd9b02b776c001d8bd
LeoPro420 <jlcontreras.18@est.ucab.edu.ve>
2021-12-14 18:04:30 -0400

    E4, B1/D.4 Employer y EmployerRegistered
    Implementada la entidad employer con su constructor y sus atributos,
    estos seránreemplazados proximamente con sus respecitvos vale
    objects. Implementado el evento de dominio EmployerRegistered, sin
    value objects ya que en los eventos de dominio estos no se utilizan

--- src/Dominio/AggRoots/Employer/Employer.ts
+++ src/Dominio/AggRoots/Employer/Employer.ts
@@ -1,17 +1,40 @@
 class Employer extends AggregateRoot implements IInternalEventHandler{
-    
-    //El constructor representa al metodo "Registrar empleador"
+    private Name:String;
+    private Description:String;
+    private Location:String;
+    private Rif:String;
+    private Phone:String;
+    private Mail:String;
+    private ComDesignation:String;
     constructor(parameters) {
        super(); 
     }
     protected When(event: object): void {
-        throw new Error("Method not implemented.");
+        switch(event){
+            case EmployeRegistered:
+                //to do (Se necesita implementar los value objects)
+                break;
+        }
     }
     protected EsureValidState(): void {
         throw new Error("Method not implemented.");
     }
 
-    public RegistrarEmpleado(){
-        this.Apply();
+    public RegistrarEmpleado(Name:String,
+        Description:String,
+        Location:String,
+        Rif:String,
+        Phone:String,
+        Mail:String,
+        ComDesignation:String){
+        this.Apply(new EmployeRegistered(
+            Name,
+            Description,
+            Location,
+            Rif,
+            Phone,
+            Mail,
+            ComDesignation
+        ));
     }
 }
---
+++ src/Dominio/DomainEvents/EmployerRegistered.ts
@@ -0,0 +1,25 @@
+class EmployeRegistered {
+    Name:String;
+    Description:String;
+    Location:String;
+    Rif:String;
+    Phone:String;
+    Mail:String;
+    ComDesignation:String;
+
+    constructor(Name:String,
+        Description:String,
+        Location:String,
+        Rif:String,
+        Phone:String,
+        Mail:String,
+        ComDesignation:String) {        
+            this.Name = Name;
+            this.Description=Description;
+            this.Location=Location;
+            this.Rif=Rif;
+            this.Phone=Phone;
+            this.Mail=Mail;
+            this.ComDesignation=ComDesignation;   
+    }
+}
