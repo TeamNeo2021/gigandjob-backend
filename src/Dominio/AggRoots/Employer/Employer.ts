85efcee119d06f28e88881490629661b14f3b060
LeoPro420 <jlcontreras.18@est.ucab.edu.ve>
2021-12-15 16:53:53 -0400

    Cambios en .eslintrc.js
    Para evitar que lance errores de formateo al utilizar "Object"

--- src/Dominio/AggRoots/Employer/Employer.ts
+++ src/Dominio/AggRoots/Employer/Employer.ts
@@ -1,44 +1,49 @@
 import { IDomainEvent } from 'src/Dominio/DomainEvents/IDomainEvent';
 import { IDomainEventHandler } from 'src/Dominio/DomainEvents/IDomainEventHandler';
 import { IInternalEventHandler } from '../IInternalEventHandler';
-import { AggregateRoot } from '../AggregateRoot'
+import { AggregateRoot } from '../AggregateRoot';
 import { EmployerRegisteredHandler } from 'src/Dominio/DomainEvents/EmployerRegisteredHandler';
-import { EmployerRegistered } from '../../DomainEvents/EmployerRegistered'
+import { EmployerRegistered } from '../../DomainEvents/EmployerRegistered';
 
-export class Employer extends AggregateRoot implements IInternalEventHandler{
-    private Name:string;
-    private Description:string;
-    private Location:string;
-    private Rif:string;
-    private Phone:string;
-    private Mail:string;
-    private ComDesignation:string;
-    constructor(parameters) {
-       super(); 
-    }
-    protected When(event: IDomainEvent, handler: IDomainEventHandler): void {
-        handler.handle(event, this);
-    }
-    protected EnsureValidState(): void {
-        console.log("protected")
-        throw new Error("Method not implemented.");
-    }
+export class Employer extends AggregateRoot implements IInternalEventHandler {
+  private Name: string;
+  private Description: string;
+  private Location: string;
+  private Rif: string;
+  private Phone: string;
+  private Mail: string;
+  private ComDesignation: string;
+  constructor(parameters) {
+    super();
+  }
+  protected When(event: IDomainEvent, handler: IDomainEventHandler): void {
+    handler.handle(event, this);
+  }
+  protected EnsureValidState(): void {
+    console.log('protected');
+    throw new Error('Method not implemented.');
+  }
 
-    public RegistrarEmpleado(Name:string,
-        Description:string,
-        Location:string,
-        Rif:string,
-        Phone:string,
-        Mail:string,
-        ComDesignation:string){
-        this.Apply(new EmployerRegistered(
-            Name,
-            Description,
-            Location,
-            Rif,
-            Phone,
-            Mail,
-            ComDesignation
-        ), new EmployerRegisteredHandler);
-    }
+  public RegistrarEmpleado(
+    Name: string,
+    Description: string,
+    Location: string,
+    Rif: string,
+    Phone: string,
+    Mail: string,
+    ComDesignation: string,
+  ) {
+    this.Apply(
+      new EmployerRegistered(
+        Name,
+        Description,
+        Location,
+        Rif,
+        Phone,
+        Mail,
+        ComDesignation,
+      ),
+      new EmployerRegisteredHandler(),
+    );
+  }
 }
