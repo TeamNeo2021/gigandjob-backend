37e74cff64f0ab8d4a78ca570b94824232b00a83
atag102 <atag102@gmail.com>
2021-12-15 19:52:30 -0400

    E4.4 Value object EmployerDescription
    Implemente el value object EmployerDescription con sus respectivos atributos y constructor.

---
+++ src/Dominio/AggRoots/Employer/ValueObjects/EmployerDescriptionVO.ts
@@ -0,0 +1,14 @@
+class EmployerDescriptionVO {
+    
+    private readonly value_employer_description: String;
+    
+    constructor(value_employer_description:String) {
+        if(value_employer_description=' '){
+            throw new Error("ERROR: La descripcion esta vacia");
+        }
+        if(value_employer_description.length>2000){
+            throw new Error("ERROR: La descripcion no puede tener mas de 2000 caracteres");
+        }
+        this.value_employer_description=value_employer_description;
+    }
+}
