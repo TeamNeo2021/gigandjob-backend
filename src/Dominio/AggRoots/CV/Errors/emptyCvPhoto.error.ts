a526b2069bc99547b4345c4e345cf07eb781ad59
Michael Nelo <syfers@gumi-os.localdomain>
2021-12-16 02:47:57 -0400

    Revert "Implementation for CV AggregateRoot Started"
    This reverts commit d54e18142eeea28b489e2ed52ef3d8957db5c61e.

--- src/Dominio/AggRoots/CV/Errors/emptyCvPhoto.error.ts
+++
@@ -1,3 +0,0 @@
-export class EmptyCvPhotoError extends Error {
-    constructor() { super("CV photo must not be empty") }
-}
--- src/Dominio/AggRoots/CV/Errors/invalidCvDescription.error.ts
+++
@@ -1,8 +0,0 @@
-export class InvalidCvDescriptionError extends Error {
-    static emptyDescription() {
-        return new InvalidCvDescriptionError("The cv description must no be empty")
-    }
-    static invalidDescriptionLength() {
-        return new InvalidCvDescriptionError("The cv description must be less than 500 characters")
-    }
-}
