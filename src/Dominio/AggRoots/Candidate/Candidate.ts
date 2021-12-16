a4409c2e999fed8ba45370f1f39fa1cb7ecc8500
Jose Luis Moncada <52092769+joselmoncada@users.noreply.github.com>
2021-12-16 02:13:06 -0400

    CandidateFullNameVo y sus validaciones
    Implementacion del patron decorador para las validaciones, no finalizado

--- src/Dominio/AggRoots/Candidate/Candidate.ts
+++ src/Dominio/AggRoots/Candidate/Candidate.ts
@@ -1,9 +1,12 @@
 import { Entity } from "src/Dominio/Core/Entity";
 import { IDomainEvent } from "src/Dominio/DomainEvents/IDomainEvent";
+import { CandidateFullNameVo } from "./ValueObjects/CandidateFullNameVo";
 import { CandidateStateVo } from "./ValueObjects/CandidateStateVo";
 
 export class Candidate extends Entity<String> {
     private _state: CandidateStateVo;
+    private _name: CandidateFullNameVo;
+ 
     
 
     protected when(event: IDomainEvent) {
@@ -17,5 +20,10 @@ export class Candidate extends Entity<String> {
     public set state(value: CandidateStateVo) {
         this._state = value;
     }
-  
+    public get name(): CandidateFullNameVo {
+        return this._name;
+    }
+    public set name(value: CandidateFullNameVo) {
+        this._name = value;
+    }
 }
