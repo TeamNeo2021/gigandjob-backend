218fdb8e763462b935da597c08a86c2a4f035130
Jorge Cróquer <jacroquer.18@est.ucab.edu.ve>
2021-12-16 13:02:14 -0400

    Entidad Application
    Solo le falta extender de entity y probar

--- src/Dominio/DomainEvents/EmployerRegisteredHandler.ts
+++ src/Dominio/DomainEvents/EmployerRegisteredHandler.ts
@@ -1,9 +1,10 @@
 import { AggregateRoot } from "../AggRoots/AggregateRoot";
+import { EmployerRegistered } from "./EmployerRegistered";
 import { IDomainEvent } from "./IDomainEvent";
 import { IDomainEventHandler } from "./IDomainEventHandler";
 
 export class EmployerRegisteredHandler implements IDomainEventHandler{
-    handle(event: IDomainEvent,entity: AggregateRoot):void{
-        console.log('hola soy la estrategia')
+    handle(event: EmployerRegistered,entity: AggregateRoot):void{
+        console.log("hola soy el handler")
     }
 }
