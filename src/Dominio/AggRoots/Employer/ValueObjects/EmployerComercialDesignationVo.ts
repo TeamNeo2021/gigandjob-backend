f300080af0f75335781836a2c60cea06064a0ed9
cristianGonz <cjgonzalezm.11@est.ucab.edu.ve>
2021-12-17 08:40:01 -0400

    E.4.12 Value Object EmployerComercialDesignationVO
    Implementado el value object EmployerComercialDesignationVO con sus respectivos atributos y constructor.

---
+++ src/Dominio/AggRoots/Employer/ValueObjects/EmployerComercialDesignationVo.ts
@@ -0,0 +1,12 @@
+export class EmployerComercialDesignationVo {
+    value: string;
+    constructor(value: string) {
+      if (value == '') {
+        throw new Error('ERROR: la denominación comercial está vacía');
+      }
+      if (value.length > 50) {
+        throw new Error('ERROR: La denominación comercial no debe ser mayor a 50 caracteres');
+      }
+      this.value = value;
+    }
+  }
