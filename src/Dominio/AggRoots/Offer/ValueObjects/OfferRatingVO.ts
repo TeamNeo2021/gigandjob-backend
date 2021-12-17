5a5368872b4c6fdf8daae697c5a5d32e3fb4994e
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2021-12-17 17:26:37 -0400

    Value Object Rating
    cambio en el Value Object

--- src/Dominio/AggRoots/Offer/ValueObjects/OfferRatingVO.ts
+++ src/Dominio/AggRoots/Offer/ValueObjects/OfferRatingVO.ts
@@ -1,9 +1,13 @@
 export class RatingVO {
-    private readonly value: boolean;
-    constructor(value: boolean) {
-      if (value == null) {
-        throw new Error('ERROR: El rating está vacío');
-      }
+    private readonly value: number;
+  
+    constructor(value: number) {
+        if (value == null) {
+            throw new Error('ERROR: El rating está vacío');
+        }
+        if (value < 0) {
+            throw new Error('ERROR: El presupuesto no debe ser menor a 0');
+        }
       this.value = value;
     }
   }
