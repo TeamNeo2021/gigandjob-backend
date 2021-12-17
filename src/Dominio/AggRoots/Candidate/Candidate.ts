687f5f2ebaafc689f35f9ad9f61b14b4594fd088
atag102 <atag102@gmail.com>
2021-12-17 13:26:14 -0400

    B3/D.1-B3/D.2 comando y evento de dominio aprovar cv aspirante
    Se implemento el comando approveCVAspirant en la entidad aspirante. Ademas de sus respectivos evento de dominio CvAspirantApproved y handler CvAspirantApprovedHandler

--- src/Dominio/AggRoots/Candidate/Candidate.ts
+++ src/Dominio/AggRoots/Candidate/Candidate.ts
@@ -1,4 +1,6 @@
 import { Entity } from "src/Dominio/Core/Entity";
+import { CvAspirantApproved } from "src/Dominio/DomainEvents/CvAspirantApproved";
+import { CvAspirantApprovedHandler } from "src/Dominio/DomainEvents/CvAspirantApprovedHandler";
 import { IDomainEvent } from "src/Dominio/DomainEvents/IDomainEvent";
 import { CandidateFullNameVo } from "./ValueObjects/CandidateFullNameVo";
 import { CandidateIdVO } from "./ValueObjects/CandidateIdVo";
@@ -8,11 +10,22 @@ export class Candidate extends Entity<String> {
     private _id: CandidateIdVO;
     private _state: CandidateStateVo;
     private _name: CandidateFullNameVo;
+    private _Cv:String;//Cambiar string por CV, tambien lo mismo con su get y set
  
     
 
     protected when(event: IDomainEvent) {
         throw new Error("Method not implemented.");
+    }
+
+    //comandos
+
+    public approveCVAspirant(Cv:String){
+        console.log("CV aprobado");
+        this.apply(
+            new CvAspirantApproved(Cv),
+            new CvAspirantApprovedHandler()
+        );
     }
 
     //getters and setters
@@ -32,4 +45,13 @@ export class Candidate extends Entity<String> {
     public set name(value: CandidateFullNameVo) {
         this._name = value;
     }
+    
+    public get Cv() : String {
+        return this.Cv;
+    }
+    
+    public set Cv(Cv : String) {
+        this.Cv = Cv;
+    }
+    
 }
