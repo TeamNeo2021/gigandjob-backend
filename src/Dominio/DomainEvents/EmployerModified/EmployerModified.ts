8941a5d4e377f546f50543abe6c11adb8131be94
cristianGonz <cjgonzalezm.11@est.ucab.edu.ve>
2021-12-17 13:47:46 -0400

    B2/D3-employerModified comments
    Comentado las partes del codigo que aún presentan problemas de dependencias debido a la falta de algunos VO en la rama principal

--- src/Dominio/DomainEvents/EmployerModified/EmployerModified.ts
+++ src/Dominio/DomainEvents/EmployerModified/EmployerModified.ts
@@ -1,9 +1,9 @@
-import { EmployerMailVo } from "src/Dominio/AggRoots/Employer/ValueObjects/EmployerMailVo";
+//import { EmployerMailVo } from "src/Dominio/AggRoots/Employer/ValueObjects/EmployerMailVo";
 import { EmployerNameVo } from "src/Dominio/AggRoots/Employer/ValueObjects/EmployerNameVo";
 import { EmployerStateVo } from "src/Dominio/AggRoots/Employer/ValueObjects/EmployerStateVo";
-import { EmployerDescriptionVO } from "src/Dominio/AggRoots/Employer/ValueObjects/EmployerDescriptionVO";
-import { EmployerRifVO } from "src/Dominio/AggRoots/Employer/ValueObjects/EmployerRifVO";
-import { EmployerLocationVO } from "src/Dominio/AggRoots/Employer/ValueObjects/EmployerLocationVO";
+//import { EmployerDescriptionVO } from "src/Dominio/AggRoots/Employer/ValueObjects/EmployerDescriptionVO";
+//import { EmployerRifVO } from "src/Dominio/AggRoots/Employer/ValueObjects/EmployerRifVO";
+//import { EmployerLocationVO } from "src/Dominio/AggRoots/Employer/ValueObjects/EmployerLocationVO";
 import { IDomainEvent } from "../IDomainEvent";
 import { EmployerComercialDesignationVo } from "src/Dominio/AggRoots/Employer/ValueObjects/EmployerComercialDesignationVo";
 
@@ -12,32 +12,32 @@ export class EmployerModified implements IDomainEvent {
     
     public dateTimeOcurred: Date;
     public Name: EmployerNameVo;
-    public Description: EmployerDescriptionVO;
+    //public Description: EmployerDescriptionVO;
     public State: EmployerStateVo;
-    public Location: EmployerLocationVO;
-    public Rif: EmployerRifVO;
+    //public Location: EmployerLocationVO;
+    //public Rif: EmployerRifVO;
     public Phone: EmployerPhoneVo;
-    public Mail: EmployerMailVo;
+    //public Mail: EmployerMailVo;
     public ComDesignation: EmployerComercialDesignationVo;    
     
     constructor(
         Name: EmployerNameVo,
-        Description: EmployerDescriptionVO,
+        //Description: EmployerDescriptionVO,
         State: EmployerStateVo,
-        Location: EmployerLocationVO,
-        Rif: EmployerRifVO,
+        //Location: EmployerLocationVO,
+        //Rif: EmployerRifVO,
         Phone: EmployerPhoneVo,
-        Mail: EmployerMailVo,
+        //Mail: EmployerMailVo,
         ComDesignation: EmployerComercialDesignationVo,
     ) {
         this.dateTimeOcurred = new Date(Date.now());
         this.Name = Name;
         this.State = State;
-        this.Description = Description;
-        this.Location = Location;
-        this.Rif = Rif;
+        //this.Description = Description;
+        //this.Location = Location;
+        //this.Rif = Rif;
         this.Phone = Phone;
-        this.Mail = Mail;
+        //this.Mail = Mail;
         this.ComDesignation = ComDesignation;
     }
 }
