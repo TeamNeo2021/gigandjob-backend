f5ebf376ed0fa4363f4c32a6359b68a1132787dd
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2021-12-17 22:20:17 -0400

    Domain Event OfferCreated
    todavia desarrollando el domain event, pero todavia tiene errores

--- src/Dominio/DomainEvents/OfferCreated/OfferCreatedHandler.ts
+++ src/Dominio/DomainEvents/OfferCreated/OfferCreatedHandler.ts
@@ -1,5 +1,9 @@
 import { Offer } from 'src/Dominio/AggRoots/Offer/Offer';
+import { BudgetVO } from 'src/Dominio/AggRoots/Offer/ValueObjects/OfferBudgetVO';
 import { DescriptionVO } from 'src/Dominio/AggRoots/Offer/ValueObjects/OfferDescriptionVO';
+import { DirectionVO } from 'src/Dominio/AggRoots/Offer/ValueObjects/OfferDirectionVO';
+import { RatingVO } from 'src/Dominio/AggRoots/Offer/ValueObjects/OfferRatingVO';
+import { SectorVO} from 'src/Dominio/AggRoots/Offer/ValueObjects/OfferSectorVO';
 import { OfferStateVO } from 'src/Dominio/AggRoots/Offer/ValueObjects/OfferStateVO';
 import { AggregateRoot } from '../../AggRoots/AggregateRoot';
 import { IDomainEvent } from '../IDomainEvent';
@@ -9,7 +13,13 @@ import { OfferCreated } from './OfferCreated';
 
 export class OfferCreatedHandler implements IDomainEventHandler {
   handle(event: OfferCreated, entity: Offer): void {
-    //entity.Description = new DescriptionVO(event.Description);
-    entity._State = new OfferStateVO(entity._State);
+    entity._State = new OfferStateVO(event.State);
+    //entity._PublicationDate = new PublicationDateVO(event.PublicationDate);
+    entity._Rating = new RatingVO(event.Rating);
+    entity._Direction = new DirectionVO(event.Direction);
+    entity._Sector = new SectorVO(event.Sector);
+    entity._Budget = new BudgetVO(event.Budget);
+    entity._Description = new DescriptionVO(event.Description);
+
   }
 }
