7dc70dd914beebcfb890c909fb9511f239c573c7
Jorge Cróquer <jacroquer.18@est.ucab.edu.ve>
2021-12-20 09:05:00 -0400

    B10/D.3
    Domain service para eliminar un aspirante despues de ser suspendido x veces

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Candidate } from './Dominio/AggRoots/Candidate/Candidate';
-import { CandidateBirthDateVo } from './Dominio/AggRoots/Candidate/ValueObjects/CandidateBirthDateVo';
-import { CandidateEmailVo } from './Dominio/AggRoots/Candidate/ValueObjects/CandidateEmailVo';
-import { CandidateFullNameVo } from './Dominio/AggRoots/Candidate/ValueObjects/CandidateFullNameVo';
-import { CandidateIdVo } from './Dominio/AggRoots/Candidate/ValueObjects/CandidateIdVo';
-import { CandidateLocationVo } from './Dominio/AggRoots/Candidate/ValueObjects/CandidateLocationVO';
-import { CandidatePhoneVo } from './Dominio/AggRoots/Candidate/ValueObjects/CandidatePhoneVo';
-import { CandidateStatesEnum, CandidateStateVo } from './Dominio/AggRoots/Candidate/ValueObjects/CandidateStateVo';
-import { CandidateEliminated } from './Dominio/DomainEvents/Candidate/CandidateEliminated';
-import { CandidateEliminatedHandler } from './Dominio/DomainEvents/Candidate/CandidateEliminatedHandler';
+
 
 
 
@@ -21,6 +12,7 @@ export class AppController {
   @Get()
   getHello(): string {
   
+
     return this.appService.getHello();
   }
 }
