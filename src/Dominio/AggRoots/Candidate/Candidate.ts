dbc5198d9495d941a29540dfb492e0ebc5e38332
joselmoncada <joseluismoncadaostos@gmail.com>
2021-12-21 23:49:41 -0400

    hotfix Candidate Switch

--- src/Dominio/AggRoots/Candidate/Candidate.ts
+++ src/Dominio/AggRoots/Candidate/Candidate.ts
@@ -4,28 +4,28 @@ import { CandidateIdVo } from "./ValueObjects/CandidateIdVo";
 import { CandidateLocationVo } from "./ValueObjects/CandidateLocationVO";
 import { CandidatePhoneVo } from "./ValueObjects/CandidatePhoneVo";
 import { CandidateEmailVo } from "./ValueObjects/CandidateEmailVo";
-import {AggregateRoot} from '../AggregateRoot'
+import { AggregateRoot } from '../AggregateRoot'
 import { Cv } from "../CV/cv.root";
 import { InvalidCandidateState } from "./ValueObjects/Errors/invalidCandidateState.error";
 import { CandidateRegisteredDomainEvent } from "../../DomainEvents/Candidate/CandidateRegistered/CandidateRegistered";
 import { CandidateStatesEnum, CandidateStateVo } from "./ValueObjects/CandidateStateVo";
-import { CandidateStateModified } from "src/Dominio/DomainEvents/Candidate/CandidateStateModified";
+import { CandidateStateModified } from "../../DomainEvents/Candidate/CandidateStateModified";
 import { InvalidCandidateAction } from "./ValueObjects/Errors/invalidCandidateAction.error";
 
 
 
 
 export class Candidate extends AggregateRoot {
- 
+
     private _id: CandidateIdVo;
     private _state: CandidateStateVo;
     private _name: CandidateFullNameVo;
-    
+
     private _phone: CandidatePhoneVo;
     private _email: CandidateEmailVo;
     private _birthDate: CandidateBirthDateVo;
     private _location: CandidateLocationVo;
-    private _Cv:Cv;
+    private _Cv: Cv;
 
     constructor(
         id: CandidateIdVo,
@@ -35,7 +35,7 @@ export class Candidate extends AggregateRoot {
         email: CandidateEmailVo,
         birthDate: CandidateBirthDateVo,
         location: CandidateLocationVo
-    ){ 
+    ) {
         super();
         this._id = id;
         this._state = state;
@@ -48,12 +48,12 @@ export class Candidate extends AggregateRoot {
 
     }
 
-    
+
     //getters and setters
 
     public get Id(): CandidateIdVo {
         return this._id;
-    } 
+    }
     public get state(): CandidateStateVo {
         return this._state;
     }
@@ -66,19 +66,19 @@ export class Candidate extends AggregateRoot {
     public set name(value: CandidateFullNameVo) {
         this._name = value;
     }
-    public get phone(): CandidatePhoneVo{
+    public get phone(): CandidatePhoneVo {
         return this._phone;
     }
 
-    public set phone(value: CandidatePhoneVo) { 
+    public set phone(value: CandidatePhoneVo) {
         this._phone = value;
     }
 
-    public get email(): CandidateEmailVo{
+    public get email(): CandidateEmailVo {
         return this._email;
     }
 
-    public get birthDay(): CandidateBirthDateVo{
+    public get birthDay(): CandidateBirthDateVo {
         return this._birthDate;
     }
 
@@ -88,32 +88,32 @@ export class Candidate extends AggregateRoot {
     public set location(value: CandidateLocationVo) {
         this._location = value;
     }
-       
-    public get Cv() : Cv {
+
+    public get Cv(): Cv {
         return this._Cv;
     }
-    
-    public set Cv(Cv : Cv) {
+
+    public set Cv(Cv: Cv) {
         this._Cv = Cv;
     }
 
- /**
-     * Checks if the Candidate state is valid and throws error when not
-     * @returns Void or InvalidCandidateState
-     */
+    /**
+        * Checks if the Candidate state is valid and throws error when not
+        * @returns Void or InvalidCandidateState
+        */
     protected EnsureValidState(): void {
-    
-        const notEmptyValues =   (
-        this._id != undefined 
-        && this._state != undefined 
-        && this._name != undefined
-        && this._phone != undefined
-        && this._email != undefined
-        && this._birthDate != undefined);
+
+        const notEmptyValues = (
+            this._id != undefined
+            && this._state != undefined
+            && this._name != undefined
+            && this._phone != undefined
+            && this._email != undefined
+            && this._birthDate != undefined);
 
         const valid = notEmptyValues //&& anotherValidation && etc
 
-        if(!valid){
+        if (!valid) {
             throw InvalidCandidateState.invalidCandidate;
         }
 
@@ -125,15 +125,15 @@ export class Candidate extends AggregateRoot {
      * it will return true only when the candidate is being instantiated with an Active State
      * @returns Boolean
      */
-    isStateValidOnRegister(){
-       
-         if( this._state.state != CandidateStatesEnum.Active){
-           
+    isStateValidOnRegister() {
+
+        if (this._state.state != CandidateStatesEnum.Active) {
+
             return false;
-         }else{
-            
-             return true;
-         }
+        } else {
+
+            return true;
+        }
 
     }
 
@@ -142,73 +142,74 @@ export class Candidate extends AggregateRoot {
      * Checks if CandidateRegister Domain Event is more thn once on the event list
      * @returns Boolean
      */
-    isCandidateAlreadyRegistered(){
-        console.log('# EVENTOS: ',this.GetChanges().length, 'lISTA: ', this.GetChanges())
+    isCandidateAlreadyRegistered() {
+        console.log('# EVENTOS: ', this.GetChanges().length, 'lISTA: ', this.GetChanges())
         let counter = 0;
         this.GetChanges().forEach(event => {
-           if(  event instanceof CandidateRegisteredDomainEvent){
-               counter++;
-           }
+            if (event instanceof CandidateRegisteredDomainEvent) {
+                counter++;
+            }
         });
-        if(counter >= 1){
-           
+        if (counter >= 1) {
+
             return true;
-        }else{
-         
+        } else {
+
             return false;
         }
     }
 
-    
-      /**
-    * @extends AggregateRoot.When
-     * Checks the type of the Domain Event to proceed 
-     * @returns Void
-     */
+
+    /**
+  * @extends AggregateRoot.When
+   * Checks the type of the Domain Event to proceed 
+   * @returns Void
+   */
     protected When(event: any): void {
-        switch(event) {
-            case event as CandidateStateModified:
-                this._state = CandidateStateVo.fromString(event.new_current)
-                console.log('new state '
-                            +event.new_current 
-                            +' applied to candidate '
-                            +this._id)
-                break;
-        
+        switch (event) {
+
             case event as CandidateRegisteredDomainEvent:
-                if(!this.isStateValidOnRegister()){
+                if (!this.isStateValidOnRegister()) {
                     console.log('register with state not active: name: ', this._name.fullName);
                     throw InvalidCandidateState.candidateStateWhenRegistering()
                 }
-                if(this.isCandidateAlreadyRegistered()){
+                if (this.isCandidateAlreadyRegistered()) {
                     console.log('already registered ', this._name.fullName)
                     throw InvalidCandidateAction.alreadyRegistered()
                 }
                 break;
-                default:
-                    console.log('The event was not identified by Candidate')
+            case event as CandidateStateModified:
+                this._state = CandidateStateVo.fromString(event.new_current)
+                console.log('new state '
+                    + event.new_current
+                    + ' applied to candidate '
+                    + this._id)
+                break;
+
+            default:
+                console.log('The event was not identified by Candidate')
                 break;
         }
     }
 
 
-       /**
-     * Register a new Candidate 
-     * @returns Candidate
-     */
+    /**
+  * Register a new Candidate 
+  * @returns Candidate
+  */
     public registerCandidate(
-        
-    ){
-        console.log('Registering Candidate #: ', this._id,'\nName: ', this._name.fullName);
+
+    ) {
+        console.log('Registering Candidate #: ', this._id, '\nName: ', this._name.fullName);
         this.Apply(new CandidateRegisteredDomainEvent(this));
         return this;
     }
 
-    public eliminateThisCandidate(){
-        console.log('Eliminating Candidate #: ', this._id,'\nName: ', this._name.fullName);
+    public eliminateThisCandidate() {
+        console.log('Eliminating Candidate #: ', this._id, '\nName: ', this._name.fullName);
         this.Apply(new CandidateStateModified('Eliminate'))
     }
-    
+
 }
 
 
