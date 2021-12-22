aab96e92a76bafeef4889c249a9d1c83f4270908
RabindraHarichand <rharichand.17@est.ucab.edu.ve>
2021-12-22 19:45:52 -0400

    Hot Fixes en meeting tests

--- src/Dominio/Tests/Meeting/meetingLocation.object.spec.ts
+++ src/Dominio/Tests/Meeting/meetingLocation.object.spec.ts
@@ -1,8 +1,9 @@
+import { EmptyMeetingLocation } from "../../AggRoots/Meeting/Errors/EmptyMeetingLocation.error"
 import { MeetingLocationVO } from "../../AggRoots/Meeting/ValueObjects/MeetingLocationVO"
 
 describe("Meeting Location",()=>{
     it("should not create when value_Meeting_location is empty",()=>{
-        expect(()=>new MeetingLocationVO(" ")).toThrowError(Error)
+        expect(()=>new MeetingLocationVO("")).toThrowError(EmptyMeetingLocation)
     })
     it(" Create when the value_Meeting_location is not empty ",()=>{
         expect(new MeetingLocationVO("Av. Teherán, Urb. Montalbán. Universidad Católica Andrés Bello.")).toBeInstanceOf(MeetingLocationVO)
