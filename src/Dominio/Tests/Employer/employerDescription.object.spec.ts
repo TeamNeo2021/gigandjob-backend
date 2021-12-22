7ad4246a3533c0ec3af3f5dad01b2723ea1deee5
anohra15 <anohra.15@est.ucab.edu.ve>
2021-12-22 14:13:40 -0400

    Test description employer
    Cambie la longitud del string de prueba para EmployerDescriptionVO

--- src/Dominio/Tests/Employer/employerDescription.object.spec.ts
+++ src/Dominio/Tests/Employer/employerDescription.object.spec.ts
@@ -5,7 +5,7 @@ describe("Employer Description",()=>{
         expect(()=>new EmployerDescriptionVO(null)).toThrowError(Error)
     })
     it("should not be created when the value is more than 500 characters",()=>{
-        let description = new Array(500).fill("a").join("")
+        let description = new Array(600).fill("a").join("")
         expect(()=>new EmployerDescriptionVO(description)).toThrowError(Error)
     })
     it(" Create when the value is less than 500 characters and is not empty ",()=>{
