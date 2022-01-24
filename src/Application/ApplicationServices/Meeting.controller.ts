011ed94759cfaa4cfcecf56bfe05e54af022a707
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2022-01-24 18:01:59 -0400

    endpoint y dto
    endpoint y dto de reject meeting junto con accept

--- src/Application/ApplicationServices/Meeting.controller.ts
+++ src/Application/ApplicationServices/Meeting.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Put, Post, Delete, Body } from '@nestjs/common';
 import { AcceptMeeting } from '../DTO/Meeting/AcceptMeeting';
+import { RejectMeeting } from '../DTO/Meeting/rejectMeetingDTO';
 import { MeetingService } from './Meeting.service';
 
 @Controller('meeting')
@@ -16,4 +17,18 @@ export class MeetingController {
     this.meetingService.Handle(request);
     return 'ok';
   }
+
+  @Put('reject')
+  rejectMeeting(
+    request: RejectMeeting,
+    @Body('candidateId') CI: string,
+    @Body('meetingId') MI: string,
+  ): any {
+    request = new RejectMeeting(CI, MI);
+    /*console.log(Body);
+    console.log(request);*/ //Esto es para probar
+    this.meetingService.Handle(request);
+    return 'ok';
+  }
+
 }
