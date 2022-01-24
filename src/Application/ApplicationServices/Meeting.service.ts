159f4490370a43fdb3c30552693ad56ce89c8a35
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2022-01-24 18:06:40 -0400

    meeting reject aplication service
    rechazar meeting como aplication service

--- src/Application/ApplicationServices/Meeting.service.ts
+++ src/Application/ApplicationServices/Meeting.service.ts
@@ -3,15 +3,24 @@ import { Meeting } from 'src/Dominio/AggRoots/Meeting/Meeting';
 import { MeetingStates } from 'src/Dominio/AggRoots/Meeting/ValueObjects/MeetingStateVO';
 import { IApplicationService } from '../Core/IApplicationService';
 import { AcceptMeeting } from '../DTO/Meeting/AcceptMeeting';
+import { RejectMeeting } from '../DTO/Meeting/rejectMeetingDTO';
 
 @Injectable()
 export class MeetingService implements IApplicationService {
   Handle(command: Object): void {
     switch (command.constructor) {
       case AcceptMeeting:
+        //agendar en calendario
         //query meeting command.meetingId
         let meeting = new Meeting();
-        meeting.state = MeetingStates.Pending;
+        meeting.state = MeetingStates.Active;
+        //query save meeting       
+        break;
+      case RejectMeeting:
+        //eliminar en calendario
+        //query meeting command.meetingId
+        let meeting = new Meeting();
+        meeting.state = MeetingStates.Canceled;
         //query save meeting
         break;
       default:
