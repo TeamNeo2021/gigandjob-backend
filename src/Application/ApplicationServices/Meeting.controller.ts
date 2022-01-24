ffa490ef2560ca97bd52a4bd6f668583305ee618
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2022-01-24 17:11:22 -0400

    Endpoint y DTO
    Dessarrollo de Endpoints y DTO de meeting, no testeado

--- src/Application/ApplicationServices/Meeting.controller.ts
+++ src/Application/ApplicationServices/Meeting.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Put, Post, Delete, Body } from '@nestjs/common';
 import { AcceptMeeting } from '../DTO/Meeting/AcceptMeeting';
+import { RejectMeeting } from '../DTO/Meeting/RejectMeetingDTO';
 import { MeetingService } from './Meeting.service';
 
 @Controller('meeting')
@@ -13,8 +14,21 @@ export class MeetingController {
     @Body('meetingId') MI: string,
   ): any {
     request = new AcceptMeeting(CI, MI);
-    console.log(Body);
-    console.log(request);
+    /*console.log(Body);
+    console.log(request);*/ //Esto es para probar
+    this.meetingService.Handle(request);
+    return 'ok';
+  }
+  //dominio/meeting/reject
+  @Put('reject')
+  rejectMeeting(
+    request: RejectMeeting,
+    @Body('candidateId') CI: string,
+    @Body('meetingId') MI: string,
+  ): any {
+    request = new RejectMeeting(CI, MI);
+    /*console.log(Body);
+    console.log(request);*/ //Esto es para probar
     this.meetingService.Handle(request);
     return 'ok';
   }
