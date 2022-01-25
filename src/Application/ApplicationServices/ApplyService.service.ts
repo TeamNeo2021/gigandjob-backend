6ca1bcac3cd2dca7611dc233bf7509d07e9feec6
LeoPro420 <jlcontreras.18@est.ucab.edu.ve>
2022-01-25 19:39:10 -0400

    Agregadas clase ApplyService y Apply to offer dto

---
+++ src/Application/ApplicationServices/ApplyService.service.ts
@@ -0,0 +1,48 @@
+import { Candidate } from 'src/Dominio/AggRoots/Candidate/Candidate';
+import { Application } from 'src/Dominio/AggRoots/Offer/Application/Application';
+import { Offer } from 'src/Dominio/AggRoots/Offer/Offer';
+import { OfferIdVO } from 'src/Dominio/AggRoots/Offer/ValueObjects/OfferIdVO';
+import { ApplyToOffer } from 'src/Dominio/DomainService/ApplyToOffer';
+import { IApplicationService } from '../Core/IApplicationService';
+import { ApplyToOfferDTO } from '../DTO/Application/ApplyToOffer.dto';
+import { ICandidateCommandRepository } from '../Repositories/CandidateCommandRepository.repo';
+import { ICandidateQuerryRepository } from '../Repositories/CandidateQuerryRepository.repo';
+import { IOfferRepository } from '../Repositories/OfferRepository.repo';
+
+export class OfferService implements IApplicationService {
+  private readonly Offerrepo: IOfferRepository;
+  private readonly CandidaterepoQ: ICandidateQuerryRepository;
+  private readonly CandidaterepoC: ICandidateCommandRepository;
+
+  constructor(
+    Offerrepo: IOfferRepository,
+    CandidaterepoQ: ICandidateQuerryRepository,
+  ) {
+    this.Offerrepo = Offerrepo;
+    this.CandidaterepoQ = CandidaterepoQ;
+  }
+
+  Handle(command: any): void {
+    switch (command.constructor) {
+      case ApplyToOffer:
+        const cmd: ApplyToOfferDTO = <ApplyToOfferDTO>command;
+        const Oferta: Offer = this.Offerrepo.load(new OfferIdVO(cmd.OfferId));
+        const Candidate: Candidate = this.CandidaterepoQ.getOne(cmd.OfferId);
+        const DSApplyToOfer: ApplyToOffer = new ApplyToOffer(
+          Candidate,
+          Oferta,
+          cmd.budget,
+          cmd.description,
+          cmd.time,
+        );
+        this.Offerrepo.save(Oferta);
+        this.CandidaterepoC.save(Candidate);
+        break;
+      default:
+        throw new Error(
+          `ApplyService: Command doesn't exist: ${command.constructor}`,
+        );
+        break;
+    }
+  }
+}
