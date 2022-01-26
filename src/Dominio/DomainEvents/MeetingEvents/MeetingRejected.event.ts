e94939b244cc184874e9b533db4109fc65a93ca6
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2022-01-26 14:08:50 -0400

    meeting reject event
    evento de rechazo de meeting

---
+++ src/Dominio/DomainEvents/MeetingEvents/MeetingRejected.event.ts
@@ -0,0 +1,13 @@
+import { IDomainEvent } from '../IDomainEvent';
+
+export class MeetingRejected implements IDomainEvent {
+  public dateTimeOcurred: Date;
+  public MeetingId: String;
+  public CandidateId: String;
+
+  constructor(MeetingId: String, CandidateId: String) {
+    this.dateTimeOcurred = new Date(Date.now());
+    this.MeetingId = MeetingId;
+    this.CandidateId = CandidateId;
+  }
+}
