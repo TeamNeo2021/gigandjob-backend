407a67a0cdd4e995e9911c79a7f75a73991acfdd
joselmoncada <joseluismoncadaostos@gmail.com>
2022-02-02 16:34:11 -0400

    api firebase

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
 
   getHello(): string {
-    return 'Hello World!';
+    return 'This is the Gig&Job Backend!';
   }
   
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as functions from "firebase-functions";
+
+let app;
 
 async function bootstrap() {
   //TODO: some function to switch repositories
-  const app = await NestFactory.create(AppModule);
+   app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  app.enableCors();
+  return app.init();
 }
-bootstrap();
+
+bootstrap()
+  .then(v => console.log('Main.ts: Nest Ready'))
+  .catch(err => console.error('Main.ts: Nest broken', err));
+
+
+export const api: functions.HttpsFunction = functions.https.onRequest(app);
+ 
