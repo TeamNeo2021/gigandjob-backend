0ba1c333b278672978abc0694cdef5e42d21f1a6
LeoPro420 <jlcontreras.18@est.ucab.edu.ve>
2022-02-03 13:42:23 -0400

    Arreglado location dto

--- src/Application/DTO/Location.dto.ts
+++ src/Application/DTO/Location.dto.ts
@@ -3,23 +3,8 @@ export class LocationDTO {
 
   public longitude: Number;
 
-<<<<<<< HEAD
   constructor(locationData: any) {
     this.latitude = locationData.latitude;
     this.longitude = locationData.longitude;
   }
 }
-=======
-    public latitude: Number;
-   
-    public longitude: Number;
-
-    constructor(locationData: any)
-    {
-        this.latitude = locationData.latitude;
-        this.longitude = locationData.longitude;
-    }
-  
- 
-}
->>>>>>> main
