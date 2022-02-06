a27d9cccffc1f4a8251a2bb15c985e419c664bf0
RabindraHarichand <rharichand.17@est.ucab.edu.ve>
2022-02-06 16:59:10 -0400

    CV-API-ENDPOINT

---
+++ src/Infrastructure/Controllers/CV/CvController.controller.ts
@@ -0,0 +1,49 @@
+import { Body, Controller, Delete, Get, HttpCode, Inject, Param, Post } from '@nestjs/common';
+import { CvService } from 'src/Application/ApplicationServices/CvService.service';
+import { RequestCvApprovalDTO } from 'src/Application/DTO/Candidate/RequestCvApproval.dto';
+
+type work = {
+    description: string,
+    startDate: string,
+    finishDate: string,
+    job: string
+}
+
+type studies = {
+    description: string
+    startDate: string
+    finishDate: string
+    institution: string
+    degree: string
+}
+
+@Controller('candidates')
+export class CVController {
+    constructor(@Inject('CvService') private service: CvService) {}
+
+    @Post()
+    @HttpCode(201)
+    RequestCVApproval(
+        @Body('CvID') cvID: string,
+        @Body('CandidateID') candidateID: string,
+        @Body('description') description: string,
+        @Body('workExperiences') workExperiences: [work],
+        @Body('studies') studies: [studies],
+        @Body('Buffer') photo: Buffer,
+        @Body('CandidateBirthdate') candidatebirthdate: string,
+    ): string {
+        let request: RequestCvApprovalDTO = new RequestCvApprovalDTO(
+            cvID,
+            candidateID,
+            description,
+            workExperiences,
+            studies, 
+            photo,
+            candidatebirthdate,
+        )
+
+        this.service.Handle(request);
+        return 'CV has been submitted for approval'
+    }
+}
+
