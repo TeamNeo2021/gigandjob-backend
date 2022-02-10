52d61e36be258c0277e683a014805bbb3fb4dd45
Jorge Cróquer <jacroquer.18@est.ucab.edu.ve>
2022-02-10 14:14:58 -0400

    HOTFIX
    Pequeno error en Meeting Creation handler. Faltaba verificar que la fecha fuera menor a late_today

--- src/Infrastructure/Event/Handlers/MeetingCreation.handler.ts
+++ src/Infrastructure/Event/Handlers/MeetingCreation.handler.ts
@@ -23,12 +23,20 @@ export class MeetingCreationHandler implements IEventHandler<MeetingScheduledEve
 
         const today = new Date();
 
-        const clean_today = new Date(today.toDateString())
+        //const clean_today = new Date(today.toDateString())
 
+        const clean_today = today;
+        clean_today.setHours(0,0,0,0)
+
+        const late_today = new Date();
+
+        late_today.setHours(23,59,58,50);
         
-
+        console.log('clean_today', clean_today.toString())
+        console.log('late_today', late_today.toString())
         const sizeQuery = await this.Meetingscollection
-            .where('date', '==', clean_today)
+            .where('date', '>=', clean_today)
+            .where('date', '<=', late_today)
             .get()
             .then(snap => 
                 meetings = snap.size)
